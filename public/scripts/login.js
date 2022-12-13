2a637bbaa8c03b65c71b12977171eee247b5919e
Theo <theo.davoust@etu.univ-tours.fr>
2022-12-13 23:01:21 +0100

    EJS de la page login, récupération du matricule + mot de passe

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,6 +1,20 @@
 const loginButton = document.getElementById('loginButton');
 
+// On submit
 loginButton.addEventListener('click', (ev) => {
 	ev.preventDefault();
-	window.location.href = "/home";
+	let matricule = document.getElementById('matricule');
+	let password = document.getElementById('password');
+
+	fetch(
+		'/login',
+		{
+			method : 'POST',
+			body : JSON.stringify({
+				matricule : matricule.value,
+				password : password.value
+			})
+		}
+	).then(function(res){
+	});
 });
