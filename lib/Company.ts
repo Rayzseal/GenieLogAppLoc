4539fb172c09b5a92372174a4ca9423303b73a15
Rayzseal <chloe.duault@laposte.net>
2022-12-13 14:04:55 +0100

    Rent system (to be tested)

--- lib/Company.ts
+++ lib/Company.ts
@@ -53,7 +53,7 @@ export class Company {
       this.removeMaterialCascade(material);
     } else if (this.rentals.find((rental) => rental.getMaterial() == material)) {
       // Can't remove employee with a dangling reference whithin locations.
-      throw "Can't delete the materiel while a location is active";
+      throw "Can't delete the material while a location is active";
     }
 
     this.materials = this.materials.filter(m => m != material);
@@ -63,14 +63,34 @@ export class Company {
     return this.rentals;
   }
 
+  /**
+   * Rent a material only if the given material if available during this period.
+   * @param rental Rent to be added. 
+   */
   addRental(rental: Rental) {
     if (!this.employees.find(e => rental.getEmployee() == e)) {
       throw "Employee doesn't exist in database";
     }
 
     if (!this.materials.find(m => rental.getMaterial() == m)) {
-      throw "Materiel doesn't exist in database";
+      throw "Material doesn't exist in database";
     }
+
+    /** 
+      * - The given starting date of rent in argument is not already during the period of rent for the same material of another rent. 
+      * - The given ending date of rent in argument is not already during the period of rent for the same material of another rent.
+      * - Check if in all the rents already added to the list, if a rent have a starting date that will occurs between the interval of the rent to be added. 
+      * - Check if in all the rents already added to the list, if a rent have a ending date that will occurs between the interval of the rent to be added.
+    */
+    this.rentals.forEach(r => {
+      if (r.getMaterial() == rental.getMaterial()) {
+        if ((this.dateInInterval(rental.getStartingDate(), r.getStartingDate(), r.getEndingDate())) 
+        || (this.dateInInterval(rental.getEndingDate(),r.getStartingDate(),r.getEndingDate()))
+        || (r.getStartingDate(), rental.getStartingDate(), rental.getEndingDate())
+        || (r.getEndingDate(), rental.getStartingDate(), rental.getEndingDate()))
+          throw new Error("Could not add rental, someone is already renting this material during this period");  
+      }
+    });
 
     this.rentals.push(rental);
   }
@@ -79,9 +99,61 @@ export class Company {
     this.rentals = this.rentals.filter(l => l == rental);
   }
 
+  /**
+   * Check if a given date is between 2 dates. 
+   * @param toBeChecked Date to be checked. 
+   * @param beginDate Begin date of interval. 
+   * @param endDate End date of interval.
+   * @returns True if the date is in the interval, false otherwise. 
+   */
+  dateInInterval(toBeChecked: Date, beginDate: Date, endDate: Date): boolean {
+    if (toBeChecked >= beginDate && toBeChecked <= endDate)
+      return true;
+    return false;
+  }
+
+  /**
+   * Check if a location is active. 
+   * @param rental Given rent to be checked. 
+   * @returns True if a location is active, false otherwise. 
+   */
+  locationIsActiveRental(rental: Rental): boolean {
+    var date = new Date();
+    if (this.dateInInterval(date,rental.getStartingDate(),rental.getEndingDate()))
+      return true;
+    return false
+  }
+
+  /**
+   * Check if a location is active for a given employee.
+   * @param emp Given employee to be checked.
+   * @returns True if a location is active, false otherwise.
+   */
+  locationIsActiveEmployee(emp: Employee): boolean {
+    this.rentals.forEach(r => {
+      if (r.getEmployee() == emp) {
+        this.locationIsActiveRental(r);
+      }
+    });  
+    return false;
+  }
+
+  /**
+   * Check if a location is active for a given material. 
+   * @param mat Given material to be checked.
+   * @returns True if a location is active, false otherwise.
+   */
+  locationIsActiveMaterial(mat: Material): boolean {
+    this.rentals.forEach(r => {
+      if (r.getMaterial() == mat) {
+        this.locationIsActiveRental(r);
+      }
+    });  
+    return false;
+  }
+
   remapClasses() {
     // This method basically remaps each Object into its corresponding class instance.
-    //TOOD verify here !! loop error
     
     this.employees = this.employees.map((e: any) => new Employee(e));
     this.materials = this.materials.map((m: any) => new Material(m));
