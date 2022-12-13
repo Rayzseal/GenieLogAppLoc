f37e20aa19f6f7f94fd06e086bf1ab5ab963531e
Theo <theo.davoust@etu.univ-tours.fr>
2022-12-13 23:07:27 +0100

    Ajout de checkInput

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -2,3 +2,11 @@ function isEmpty(str)
 {
   return typeof str === 'string' && (str === '' || !str.trim());
 }
+
+function checkInput(input)
+{
+  if(isEmpty(input.value))
+    input.classList.add('input-error');
+  else
+    input.classList.remove('input-error');
+}
