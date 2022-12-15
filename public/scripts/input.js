fc4e0d7f424d2ddcf83d46787c7631fd7c4674fb
Theo <theo.davoust@etu.univ-tours.fr>
2022-12-15 23:10:47 +0100

    inputCheck

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -1,12 +1,14 @@
-function isEmpty(str)
-{
+// Export the `isEmpty` function
+function isEmpty(str) {
   return typeof str === 'string' && (str === '' || !str.trim());
 }
 
-function checkInput(input)
-{
-  if(isEmpty(input.value))
-    input.classList.add('input-error');
+// Export the `checkInput` function
+function checkInput(input) {
+  let ret = isEmpty(input.value);
+  if(ret)
+    input.className += ' input-error';
   else
-    input.classList.remove('input-error');
+    input.className = input.className.replace(/\binput-error\b/,'');
+  return ret;
 }
