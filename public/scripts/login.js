9234bf26a13f0742bb5fd25077a5161d5226727b
Theo <theo.davoust@etu.univ-tours.fr>
2022-12-22 15:58:53 +0100

    ajout de toat et envoie des données post pour le login

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -3,30 +3,31 @@ const loginButton = document.getElementById("loginButton");
 // On submit
 loginButton.addEventListener("click", (ev) => {
 	ev.preventDefault();
-	let matricule = document.getElementById("matricule");
-	let password  = document.getElementById("password");
+	let matricule = document.querySelector('#matricule');
+	let password  = document.querySelector('#password');
 
 
 	let result_matricule = checkInput(matricule);
 	let result_password  = checkInput(password);
 	if (result_matricule || result_password) {
-		const errorPopup = new Popup(getErrorFlow());
-		return errorPopup.open();
+		toaster.display(getErrorFlow(), 'orange');
 	}
-
-	fetch(
-		'/login',
-		{
-			method : 'POST',
-			body : JSON.stringify({
-				matricule : matricule.value,
-				password : password.value
-			})
-		}
-	).then(function(res){
-		// If no error, redirect to app
-		document.location.href = "/home";
-	}).catch(function(res){
-		console.log(res);
-	});
+	else{
+		fetch(
+			'/login',
+			{
+				method : 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body : JSON.stringify({
+					matricule : matricule.value,
+					password : password.value
+				})
+			}
+		).then(function(res){
+			// If no error, redirect to app
+			document.location.href = '/home';
+		}).catch(function(res){
+			toaster.display('La requête n\'a pas aboutie');
+		});
+	}
 });
