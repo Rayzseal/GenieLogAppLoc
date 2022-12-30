59ce869e07d8784734021907d0b328b6c58ca370
TSnake41 <TSnake@Outlook.fr>
2022-12-30 23:16:20 +0100

    Ajout des modifications sur Database, enlever NodeJS v14 des tests (incompatible avec les tests selenium)

--- lib/Database.ts
+++ lib/Database.ts
@@ -1,21 +1,23 @@
-import {readFileSync, writeFileSync} from "fs";
-import {Company} from "./Company";
+import { readFileSync, writeFileSync } from "fs";
+import { Company } from "./Company";
 
-const DB_FILE = "db.json";
+const DEFAULT_DB_FILE = "db.json";
 
 export class Database {
 	private readonly company: Company;
+	private readonly path: undefined | string;
 
-	constructor(company: Company) {
+	constructor(company: Company, path: undefined | string = undefined) {
 		this.company = company;
+		this.path = path;
 	}
 
-	static load(path: string = DB_FILE) {
+	static load(path: string = DEFAULT_DB_FILE) {
 		try {
 			const company = Object.assign(new Company(), JSON.parse(readFileSync(path).toString()));
 			company.remapClasses();
 
-			return new Database(company);
+			return new Database(company, path);
 		} catch (error) {
 			console.log(`Can't load database: ${error}, use an empty database instead.`);
 
@@ -27,7 +29,9 @@ export class Database {
 	 * Write the application datas into a file in order to keep those even when the app is turned off.
 	 * @param path A specific path where to write the file to.
 	 */
-	saveToFile(path: string = DB_FILE) {
-		writeFileSync(path, JSON.stringify(this.company, null, 4));
+	saveToFile() {
+		if (this.path !== undefined) {
+			writeFileSync(this.path, JSON.stringify(this.company, null, 4));
+		}
 	}
 }
