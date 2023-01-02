047c29b6b698cc76e212d31a85f341c58a2c2e44
Theo <theo.davoust@etu.univ-tours.fr>
2023-01-02 20:50:26 +0100

    Filter Employee + Material

---
+++ public/scripts/employee/filterEmployee.js
@@ -0,0 +1,21 @@
+const material_list = document.querySelector("ul")
+const input = document.querySelector("#filter_input")
+
+function isEmpty(str) {
+	return typeof str === "string" && (str === "" || !str.trim());
+}
+
+function filter(text){
+  var items = material_list.getElementsByTagName("li");
+  for (i = 0; i < items.length; i++) {
+    var content = items[i].querySelector(".employe_name");
+    if(!String(content.innerHTML).toLowerCase().includes(text.toLowerCase()))
+      items[i].style.display = "none";
+    else
+      items[i].style.display = "";
+  }
+}
+
+input.addEventListener("input", function(){
+  filter(input.value);
+});
